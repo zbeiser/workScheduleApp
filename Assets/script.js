747ea542e373edb33f8fbafde00530cf42854811
Zach Beiser <zbeiser@gmail.com>
2022-10-31 08:57:12 -0700

    deleted button icon spans and added their class to the buttons. Events array now full of strings, not arrays. condensed if (loadEvents) statment

--- Assets/script.js
+++ Assets/script.js
@@ -1,52 +1,36 @@
 // Declare DOM hooks
-var currentDayEl = $('#currentDay');
-var saveEventBtnEl = $('.saveBtn');
-var textAreaEl = $('.description');
+var currentDayEl = $("#currentDay");
+var saveEventBtnEl = $(".saveBtn");
+var textAreaEl = $(".description");
 
 // Declare state variables
-var eventsArray = [
-  [""],
-  [""],
-  [""],
-  [""],
-  [""],
-  [""],
-  [""],
-  [""],
-  [""],
-];
+var eventsArray = ["", "", "", "", "", "", "", "", ""];
 
 // Display the current day at top of page
 function displayCurrentDay() {
-  var currentDay = moment().format('dddd, MMM DD, YYYY');
+  var currentDay = moment().format("dddd, MMM DD, YYYY");
   currentDayEl.text(currentDay);
 }
 
-var currentTime = moment().format('H');
+// Changes colors of textareas in reference to past, present, or future hours.
+var currentTime = moment().format("H");
 
 for (var i = 0; i < textAreaEl.length; i++) {
   if (Number(currentTime) > Number(textAreaEl[i].id)) {
-    textAreaEl[i].classList.add('past');
-  }
-  else if (Number(currentTime) === Number(textAreaEl[i].id)) {
-    textAreaEl[i].classList.add('present');
-  }
-  else if (Number(currentTime) < Number(textAreaEl[i].id)) {
-    textAreaEl[i].classList.add('future');
+    textAreaEl[i].classList.add("past");
+  } else if (Number(currentTime) === Number(textAreaEl[i].id)) {
+    textAreaEl[i].classList.add("present");
+  } else if (Number(currentTime) < Number(textAreaEl[i].id)) {
+    textAreaEl[i].classList.add("future");
   }
 }
 
 // Save event inside time-block textarea to localStorage
 function saveEvent(ev) {
-  var saveClick = ev.target
-  // Retargets button if they click the span save icon
-  if (saveClick.className == "fas fa-save") {
-    saveClick = saveClick.parentNode
-  }
-  
+  var saveClick = ev.target;
   var eventText = saveClick.previousElementSibling.value;
   // Hooks save buttons up to their respective textareas
-  for (var i = 0 ; i < eventsArray.length; i++) {
+  for (var i = 0; i < eventsArray.length; i++) {
     if (i == saveClick.id) {
       eventsArray[i] = eventText;
     }
@@ -56,34 +40,23 @@ function saveEvent(ev) {
 }
 
 // Adds click event listener to all save buttons
-for (var i = 0 ; i < saveEventBtnEl.length; i++) {
-  saveEventBtnEl[i].addEventListener('click', saveEvent); 
+for (var i = 0; i < saveEventBtnEl.length; i++) {
+  saveEventBtnEl[i].addEventListener("click", saveEvent);
 }
 
 // Load events saved in local storage to the time-blocks' textarea upon webpage loading.
-// First if/else prevents null reading if there's no localStorage saved. 
+// First if statement prevents null reading if there's no localStorage saved.
 function updateEvents() {
   var loadEvents = JSON.parse(localStorage.getItem("Stored Events"));
-  if (loadEvents === null) {
-    eventsArray = [
-      [""],
-      [""],
-      [""],
-      [""],
-      [""],
-      [""],
-      [""],
-      [""],
-      [""],
-    ];
-  } else {
+
+  if (loadEvents) {
     eventsArray = loadEvents;
   }
 
-  // Loads eventsArray index values up to their respective textareas. 
-  for (var i = 0 ; i < eventsArray.length; i++) {
+  // Loads eventsArray index values up to their respective textareas.
+  for (var i = 0; i < eventsArray.length; i++) {
     var loadText = document.getElementById(i + 9);
-    if (i == (loadText.id - 9)) {
+    if (i == loadText.id - 9) {
       loadText.value = eventsArray[i];
     }
   }
